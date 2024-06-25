7f42b7285ffc9bcb4e9ee542acc4efcb3a5ad636
faizan Shaikh <fs61faizan@gmail.com>
2024-06-25 18:44:37 +0530

    delete dialog for all

--- Components/CustomerCard.js
+++ Components/CustomerCard.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Card, Text } from "react-native-paper";
 import { IconButton, Icon } from "react-native-paper";
 import { StyleSheet,View } from "react-native";
+import DeleteModal from '../UI/DeleteModal';
 export default function CustomerCard({customer,navigation}) {
+  const[visible,setVisible]= useState(false);
+  const [customerId,setCustomerId]=useState('')
+  function deleteCustomerDelete(id) {
+    setCustomerId(id);
+    setVisible(true);
+  }
+  const handleDelete = async () => {
+    // Your delete logic here
+    console.log(customerId);
+    // setInvoices((prev)=>prev.filter((item) => item.id !== invoiceId));
+    setVisible(false);
+    try{
+      const response= await fetch(`http://192.168.1.2:8888/api/people/delete/${customerId}`, {
+          method: 'DELETE',
+          credentials:'include'
+        })
+        if (response.ok) {
+              console.log("item delted")
+              
+            } else {
+              console.error('Failed to delete the item');
+            }
+          } catch (error) {
+            console.error('Error:', error);
+          }    
+  };
   function customerDetail(id){
     navigation.navigate("CustomerDetail", {
       customerId:id
     })
+   
 }
   return (
    <View>
@@ -29,8 +57,15 @@ export default function CustomerCard({customer,navigation}) {
                icon="delete"
                iconColor="#1976d2"
                size={20}
-               onPress={() => console.log("Pressed")}
+               onPress={() => deleteCustomerDelete(item._id)}
              />
+              {visible && (
+                <DeleteModal
+                  visible={visible}
+                  setVisible={setVisible}
+                  handleDelete={handleDelete}
+                />
+              )}
              <Button style={{ backgroundColor: "#1976d2" }}>
                <Icon source="pencil" color="white" size={20} /> Edit
              </Button>
--- Components/ProductCard.js
+++ Components/ProductCard.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Button, Card, Text } from "react-native-paper";
 import { IconButton, Icon } from "react-native-paper";
 import { StyleSheet,View } from "react-native";
+import DeleteModal from '../UI/DeleteModal';
 function ProductCard({products,navigation}) {
+  const [visible, setVisible] = useState(false);
+  const [productId, setProductId] = useState("");
+  function deleteProductHandler(id) {
+    setProductId(id);
+    setVisible(true);
+  }
+
+  const handleDelete = async () => {
+    // Your delete logic here
+    console.log(productId);
+    // setInvoices((prev)=>prev.filter((item) => item.id !== productId));
+    setVisible(false);
+    try{
+      const response= await fetch(`http://192.168.1.2:8888/api/product/delete/${productId}`, {
+          method: 'DELETE',
+          credentials:'include'
+        })
+        if (response.ok) {
+              console.log("item delted")
+              
+            } else {
+              console.error('Failed to delete the item');
+            }
+          } catch (error) {
+            console.error('Error:', error);
+          }    
+  }
   function ProductDetail(id){
     navigation.navigate("ProductDetail", {
       productId:id
@@ -33,8 +61,15 @@ function ProductCard({products,navigation}) {
                icon="delete"
                iconColor="#1976d2"
                size={20}
-               onPress={() => console.log("Pressed")}
+               onPress={() => deleteProductHandler(item._id)}
              />
+             {visible && (
+                <DeleteModal
+                  visible={visible}
+                  setVisible={setVisible}
+                  handleDelete={handleDelete}
+                />
+              )}
              <Button style={{ backgroundColor: "#1976d2" }}>
                <Icon source="pencil" color="white" size={20} /> Edit
              </Button>
