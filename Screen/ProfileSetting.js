1cd6666fb62045555c7b4254a5201e90b457f6d1
faizan Shaikh <fs61faizan@gmail.com>
2024-06-29 15:07:56 +0530

    login detail issue

--- Screen/ProfileSetting.js
+++ Screen/ProfileSetting.js
@@ -1,10 +1,35 @@
 
-import { useContext } from 'react';
+import { useContext,useEffect,useState } from 'react';
 import { StyleSheet,View } from 'react-native';
-import { Card, Title, Paragraph,Button,Avatar } from 'react-native-paper';
+import { Card, Title, Paragraph,Button,Avatar, ActivityIndicator } from 'react-native-paper';
 import { AuthContext } from '../Store/AuthContext';
+import { useIsFocused } from '@react-navigation/native';
 function ProfileSetting() {
-    const{loginDetail}=useContext(AuthContext)
+    const{loginDetail,getData}=useContext(AuthContext)
+    const [loginDetail1, setLoginDetail1] = useState(loginDetail);
+    const [newLoading,setNewLoading] =useState(true)
+    const isFocused=useIsFocused()
+    
+  useEffect(() => {
+    async function loginDetailHandler() {
+      try{
+      const newValue = await getData("loginDetail")||'';
+      
+      setLoginDetail1(newValue);
+      }catch{
+        console.log("failed get data ")
+      }
+      finally{
+        setNewLoading(false)
+      }
+    }
+
+    loginDetailHandler();
+  }, [isFocused,loginDetail]);
+  const login= loginDetail1
+  if(newLoading){
+    return <ActivityIndicator size="large"/>
+  }
   return (
     <View style={styles.container}>
     <Card style={styles.card}>
@@ -14,12 +39,12 @@ function ProfileSetting() {
           style={styles.avatar}
         />
         <Card.Content style={{alignSelf:'center'}}>
-          <Title style={styles.titleStyle}>{`${loginDetail.name} ${loginDetail.surname}`}</Title>
-          <Paragraph style={styles.paragraph}>ID: {loginDetail._id}</Paragraph>
-          <Paragraph style={styles.paragraph}>Name: {loginDetail.name}</Paragraph>
-          <Paragraph style={styles.paragraph}>Surname: {loginDetail.surname}</Paragraph>
-          <Paragraph style={styles.paragraph}>Email: {loginDetail.email}</Paragraph>
-          <Paragraph style={styles.paragraph}>Role: {loginDetail.role}</Paragraph>
+          <Title style={styles.titleStyle}>{`${login.name} ${login.surname}`}</Title>
+          <Paragraph style={styles.paragraph}>ID: {login._id}</Paragraph>
+          <Paragraph style={styles.paragraph}>Name: {login.name}</Paragraph>
+          <Paragraph style={styles.paragraph}>Surname: {login.surname}</Paragraph>
+          <Paragraph style={styles.paragraph}>Email: {login.email}</Paragraph>
+          <Paragraph style={styles.paragraph}>Role: {login.role}</Paragraph>
         </Card.Content>
         <Card.Actions>
         <Button
