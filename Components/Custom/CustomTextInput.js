0595247081593d1ee930af13e9698b763fd63073
prathmeshlakhade <prathmeshlakhade@gmail.com>
2024-08-01 10:55:03 +0530

    complete view shop list and create shop

---
+++ Components/Custom/CustomTextInput.js
@@ -0,0 +1,47 @@
+// CustomTextInput.js
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TextInput, HelperText } from 'react-native-paper';
+
+const CustomTextInput = ({
+  label,
+  value,
+  onChangeText,
+  onBlur,
+  error,
+  touched,
+  keyboardType = 'default',
+}) => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        label={label}
+        mode="outlined"
+        value={value}
+        onChangeText={onChangeText}
+        onBlur={onBlur}
+        error={touched && error ? true : false}
+        style={styles.textInput}
+        keyboardType={keyboardType}
+      />
+      {touched && error && (
+        <HelperText type="error" visible={touched && error}>
+          {error}
+        </HelperText>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    marginBottom: 10,
+  },
+  textInput: {
+    width: '100%',
+    marginBottom: 10,
+  },
+});
+
+export default CustomTextInput;
