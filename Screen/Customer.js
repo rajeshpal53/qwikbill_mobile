2971139fa93aa92470286b6c40559e4cf4e6cd27
faizan Shaikh <fs61faizan@gmail.com>
2024-08-08 11:53:39 +0530

    new changes

--- Screen/Customer.js
+++ Screen/Customer.js
@@ -92,7 +92,7 @@ export default function Customer({ navigation }) {
     formData.append('shop',shopDetails._id);
 
     try {
-      const response = await axios.post('http://192.168.1.6:8888/api/people/upload', formData, {
+      const response = await axios.post('http://192.168.1.5:8888/api/people/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
