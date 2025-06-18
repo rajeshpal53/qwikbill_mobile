316e052c0dca0b6edc6e6964ccf8b8be38adc1f8
[jayesh satnurkar] <[jayeshsatnurkar1@gmail.com]>
2025-06-18 19:14:31 +0530

    create invoice functionality issue resolved

---
+++ Qwikbill/src/Component/CustomBackButton.js
@@ -0,0 +1,17 @@
+// // CustomBackButton.js
+// import React from 'react';
+// import { TouchableOpacity } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
+
+// const CustomBackButton = ({marginTop=0}) => {
+//   const navigation = useNavigation();
+
+//   return (
+//     <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft:10, justifyContent:"center",width:48,height:48,marginTop:marginTop}}>
+//         <Icon name="chevron-left" size={40} color="#fff" />
+//     </TouchableOpacity>
+//   );
+// };
+
+// export default CustomBackButton;
